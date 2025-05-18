0ef5e1f392a5bdd3da131920fe114f482dfa75b0
SamirCoder16 <samirparvez48@gmail.com>
2025-05-18 09:54:24 +0530

    server is cleared and vercel is deleted

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,12 +3,10 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import express from 'express';
 import morgan from 'morgan'
-import path from 'path'
 
 // PORT configeration
 const PORT = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
 
 // Import MongoDB and connect to Server to Running .
 import { connectDB } from './lib/db.js'
@@ -40,13 +38,6 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/chat', chatRoutes)
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-    app.get("*", (req,res)=> {
-        res.sendFile(path.join(__dirname,  "../Frontend", "dist", "index.html"))
-    })
-}
 // Listen the Server .
 app.listen(PORT, ()=>{
     console.log(`server is running on port ============> ${PORT}`);
