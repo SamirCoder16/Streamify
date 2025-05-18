6354961553e608615ab0a2e6d05cb92cc6b85396
Samir Parvez <192109895+SamirCoder16@users.noreply.github.com>
2025-05-18 10:21:39 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : 'https://streamify-backend-5j04.onrender.com';
+const BASE_URL = import.meta.env.MODE === "development" ? 'http://localhost:5001/api' : '/api';
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
